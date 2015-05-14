a39e47a4297032c7dd835d9e73f16536670e931f
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-05-14 17:19:01 -0400

    Separate out models and parser from the main

---
+++ main.py
@@ -0,0 +1,23 @@
+from xml.etree import ElementTree
+from parser import *
+import sys
+
+
+def main():
+    args = sys.argv
+
+    if len(args) != 2:
+        print 'ERROR: Pass in the xml file name'
+        exit(1)
+
+    try:
+        tree = ElementTree.parse(args[1])
+        procedureElement = tree.getroot()
+        procedure = Parser.parseProcedure(procedureElement)
+
+    except IOError:
+        print 'ERROR: Invalid filename'
+        exit(1)
+
+if __name__ == '__main__':
+    main()
